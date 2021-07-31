cc2efc453ae903c99549394f4e8d545af4735c96
shadowninja55 <shadowninja1050@gmail.com>
2021-07-30 20:51:55 -0400

    use unstable instead

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   outputs = { self, nixpkgs }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
     defaultPackage.x86_64-linux = pkgs.grapejuice.override {
-      wine = pkgs.wineWowPackages.staging.overrideAttrs (
+      wine = pkgs.wineWowPackages.unstable.overrideAttrs (
         old: {
           patches = old.patches ++ [ ./patches/mouse.patch ]; 
         }
